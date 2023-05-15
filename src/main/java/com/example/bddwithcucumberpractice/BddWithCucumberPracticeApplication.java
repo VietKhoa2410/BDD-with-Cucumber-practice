a53d925ce65778e0669995e9c5dd778354eb60d2
khoado <kdo@paramountcommerce.com>
2023-05-15 14:30:52 +0700

    init

---
+++ src/main/java/com/example/bddwithcucumberpractice/BddWithCucumberPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bddwithcucumberpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddWithCucumberPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddWithCucumberPracticeApplication.class, args);
+	}
+
+}
